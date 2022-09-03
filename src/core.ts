a66883d09255591ef9675f203c793386d862a676
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-09-04 00:59:13 +0500

    style

--- src/core.ts
+++ src/core.ts
@@ -3,5 +3,5 @@ import { NativeModules } from 'react-native';
 const CoreNativeApi = NativeModules.TuyaCoreModule;
 
 export function initWithOptions(params: { appKey: string, appSecret: string }) {
-  CoreNativeApi.initWithOptions(params);
+	CoreNativeApi.initWithOptions(params);
 }
