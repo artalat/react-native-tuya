a091aec2c8ea3ab388789dadbc9c633b934fe5b2
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-09-03 20:05:07 +0500

    chore: Updated .kt custom module

--- android/src/main/java/expo/modules/tuya/TuyaPackage.kt
+++ android/src/main/java/expo/modules/tuya/TuyaPackage.kt
@@ -1,13 +1,13 @@
-package expo.modules.custom
+package expo.modules.ReactNativeTuya
 
 import android.content.Context
 import expo.modules.core.BasePackage
+import expo.modules.core.ExportedModule
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
+import expo.modules.core.interfaces.SingletonModule
 
-class TuyaPackage : BasePackage() {
+class ReactNativeTuyaPackage : BasePackage() {
   override fun createReactActivityLifecycleListeners(activityContext: Context): List<ReactActivityLifecycleListener> {
-    return listOf(CustomReactActivityLifecycleListener(activityContext))
+    return listOf(ReactNativeTuyaReactActivityLifecycleListener(activityContext))
   }
-
-  // ...
 }
--- android/src/main/java/expo/modules/tuya/TuyaReactActivityLifecycleListener.kt
+++ android/src/main/java/expo/modules/tuya/TuyaReactActivityLifecycleListener.kt
@@ -1,4 +1,4 @@
-package expo.modules.custom
+package expo.modules.ReactNativeTuya
 
 import android.app.Activity
 import android.content.Context
@@ -7,13 +7,8 @@ import android.util.Log
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
 import com.tuya.smart.rnsdk.core.TuyaCoreModule;
 
-class CustomReactActivityLifecycleListener(activityContext: Context) : ReactActivityLifecycleListener {
+class ReactNativeTuyaReactActivityLifecycleListener(activityContext: Context) : ReactActivityLifecycleListener {
   override fun onCreate(activity: Activity, savedInstanceState: Bundle?) {
     TuyaCoreModule.Companion.initTuyaSDKWithoutOptions(this);
   }
-
-  // Naming is node module name (`expo-custom`) plus value name (`value`) using underscores as a delimiter
-  // i.e. `expo_custom_value`
-  // `@expo/vector-icons` + `iconName` -> `expo__vector_icons_icon_name`
-  private fun getValue(context: Context): String = context.getString(R.string.expo_custom_value).toLowerCase()
 }
