1555a5021f242b01780f4839bf705885346b18e6
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-09-03 16:08:48 +0500

    feat: Added Expo Plugin

---
+++ plugin/src/withTuyaOnAndroid.ts
@@ -0,0 +1,43 @@
+import { AndroidConfig, ConfigPlugin, withAndroidManifest } from '@expo/config-plugins';
+import { ExpoConfig } from '@expo/config-types';
+
+import { Props } from './withTuya';
+
+// Using helpers keeps error messages unified and helps cut down on XML format changes.
+const { addMetaDataItemToMainApplication, getMainApplicationOrThrow } = AndroidConfig.Manifest;
+
+export const withTuyaOnAndroid: ConfigPlugin<Props> = (config, props) => {
+	return withAndroidManifest(config, async config => {
+		// Modifiers can be async, but try to keep them fast.
+		config.modResults = await setCustomConfigAsync(config as any, config.modResults, props);
+		return config;
+	});
+};
+
+// Splitting this function out of the mod makes it easier to test.
+async function setCustomConfigAsync(
+	config: Pick<ExpoConfig, 'android'>,
+	androidManifest: AndroidConfig.Manifest.AndroidManifest,
+	props: Props
+): Promise<AndroidConfig.Manifest.AndroidManifest> {
+	// Get the <application /> tag and assert if it doesn't exist.
+	const mainApplication = getMainApplicationOrThrow(androidManifest);
+
+	addMetaDataItemToMainApplication(
+		mainApplication,
+		// value for `android:name`
+		'TUYA_SMART_APPKEY',
+		// value for `android:value`
+		props.apiKey
+	);
+
+	addMetaDataItemToMainApplication(
+		mainApplication,
+		// value for `android:name`
+		'TUYA_SMART_SECRET',
+		// value for `android:value`
+		props.apiSecret
+	);
+
+	return androidManifest;
+}
---
+++ plugin/src/withTuyaOnIos.ts
@@ -0,0 +1,92 @@
+import { ConfigPlugin, withAppDelegate, withEntitlementsPlist, withInfoPlist } from '@expo/config-plugins';
+import { mergeContents, MergeResults } from '@expo/config-plugins/build/utils/generateCode';
+
+import { Props } from './withTuya';
+
+/**
+ * Apply react-native-tuya configuration for Expo SDK 44 projects.
+ */
+export const withTuyaOnIos: ConfigPlugin<Props> = (
+	config,
+	props
+) => {
+	withTuyaAppDelegate(config, props);
+	withTuyaEntitlements(config);
+
+	return withReactNativeTuyaInfoPlist(config, props);
+};
+
+const withReactNativeTuyaInfoPlist: ConfigPlugin<Props> = (
+	config,
+	activityTypes
+) => {
+	return withInfoPlist(config, (config) => {
+		config.modResults.NSUserActivityTypes = activityTypes;
+		return config;
+	});
+};
+
+export function addTuyaAppDelegateImport(src: string): MergeResults {
+	return mergeContents({
+		tag: 'react-native-tuya',
+		src,
+		newSrc: '#import <TuyaSmartHomeKit/TuyaSmartKit.h>',
+		anchor: /#import "AppDelegate\.h"/,
+		offset: 1,
+		comment: '//',
+	});
+}
+
+export function addTuyaAppDelegateInit(src: string, props: Props): MergeResults {
+	return mergeContents({
+		tag: 'react-native-tuya-delegate',
+		src,
+		newSrc: [
+			'	#ifdef DEBUG',
+			'    [[TuyaSmartSDK sharedInstance] setDebugMode:YES];',
+			'  #endif',
+			'',
+			`  [[TuyaSmartSDK sharedInstance] startWithAppKey:@"${props.apiKey}" secretKey:@"${props.apiSecret}"];`,
+		].join('\n'),
+		anchor:
+      / {2}UIView *rootView = [self.reactDelegate createRootViewWithBridge:bridge moduleName:@"main" initialProperties:nil];/,
+		offset: -1,
+		comment: '//',
+	});
+}
+
+/** Append the Tuya entitlement on iOS */
+const withTuyaEntitlements: ConfigPlugin = (config) => {
+	return withEntitlementsPlist(config, (config) => {
+		config.modResults['com.apple.developer.networking.multicast'] = true;
+		return config;
+	});
+};
+
+const withTuyaAppDelegate: ConfigPlugin<Props> = (config, props) => {
+	return withAppDelegate(config, (config) => {
+		if (['objc', 'objcpp'].includes(config.modResults.language)) {
+			try {
+				config.modResults.contents = addTuyaAppDelegateImport(
+					config.modResults.contents
+				).contents;
+				config.modResults.contents = addTuyaAppDelegateInit(
+					config.modResults.contents,
+					props
+				).contents;
+			} catch (error) {
+				if ((error as any).code === 'ERR_NO_MATCH') {
+					throw new Error(
+						'Cannot add Tuya Library to the project\'s AppDelegate because it\'s malformed. Please report this with a copy of your project AppDelegate.'
+					);
+				}
+				throw error;
+			}
+		} else {
+			throw new Error(
+				'Cannot setup Tuya Library because the AppDelegate is not Objective C'
+			);
+		}
+		return config;
+	});
+};
